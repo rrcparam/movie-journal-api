43537944a32d3abd9c03f7659a06d5a3b94ae41e
Paramdeep Singh <psingh20@rrc.ca>
2025-11-10 19:58:32 -0600

    done the  setup of  server and base API configuration

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import movieRoutes from "./api/routes/movieRoutes";
+import swaggerRoutes from "./config/swagger";
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use("/api/v1/docs", swaggerRoutes);
+
+// Health Check
+app.get("/api/v1/health", (req: Request, res: Response) => {
+  res.json({
+    status: "OK",
+    service: " Movie Journal API",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0",
+  });
+});
+
+// This is for Register all main routes
+app.use("/api/v1/movies", movieRoutes);
+
+// Welcome route
+
+app.get("/api/v1", (req: Request, res: Response) => {
+  res.json({
+    message: " Welcome to the Movie Journal API",
+    routes: {
+      movies: "/api/v1/movies",
+      reviews: "/api/v1/reviews",
+      watchlists: "/api/v1/watchlists",
+      health: "/api/v1/health",
+    },
+  });
+});
+
+export default app;
