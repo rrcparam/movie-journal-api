d04156468dc51126d6da8470aa6c506590093c39
Paramdeep Singh <psingh20@rrc.ca>
2025-11-24 22:52:44 -0600

    adding the  OpenAPI documentation for watchlist endpoints

--- src/api/v1/routes/watchlistRoutes.ts
+++ src/api/v1/routes/watchlistRoutes.ts
@@ -4,11 +4,40 @@ import {
   addToWatchlist,
   removeFromWatchlist,
 } from "../controllers/watchlistController";
-
+import { authenticate } from "../middleware/authenticate";
 const router = Router();
 
+
+/**
+ * @openapi
+ * /watchlist:
+ *   get:
+ *     summary: Get user's watchlist
+ *     tags: [Watchlist]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of watchlist items
+ *
+ *   post:
+ *     summary: Add movie to watchlist
+ *     tags: [Watchlist]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/WatchlistCreateInput"
+ *     responses:
+ *       201:
+ *         description: Added to watchlist
+ */
+
 router.get("/", getWatchlist);
-router.post("/", addToWatchlist);
-router.delete("/:id", removeFromWatchlist);
+router.post("/", authenticate,  addToWatchlist);
+router.delete("/:id", authenticate,  removeFromWatchlist);
 
 export default router;
